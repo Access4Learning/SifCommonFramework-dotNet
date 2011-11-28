3d6a21fb9168fd3c8059f0738ff7a2fb12e191af
Rafidzal Rafiq <rafidzal.rafiq@systemic.com.au>
2011-11-28 10:45:31 +0800

    Added convenience batch scripts for running the demos.

---
+++ Scripts/bat/Subscribing.Print.bat
@@ -0,0 +1,28 @@
+@echo off
+
+rem ============================================================================
+rem == User defined environment variables                                     ==
+rem ============================================================================
+
+set EXECUTABLE_PATH=\soe\Projects\SifCommonFramework\Code\SifCommonFramework\Systemic.Sif.Demo.Subscribing.Print\bin\Release\
+set EXECUTABLE=Systemic.Sif.Demo.Subscribing.Print.exe
+
+echo EXECUTABLE=%EXECUTABLE_PATH%%EXECUTABLE%
+
+rem ============================================================================
+rem == Safety checks                                                          ==
+rem ============================================================================
+
+if exist %EXECUTABLE_PATH%%EXECUTABLE% goto okExec
+echo Could not find : %EXECUTABLE_PATH%%EXECUTABLE%
+pause
+goto end
+:okExec
+
+rem ============================================================================
+rem == Start executable
+rem ============================================================================
+
+title %EXECUTABLE%
+start /D %EXECUTABLE_PATH% /WAIT /B %EXECUTABLE%
+:end
